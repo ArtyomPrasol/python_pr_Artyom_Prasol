05493611b4b11e1d88edbb0a79cc4cd4a1f4c036
ArtyomPrasol <112947624+ArtyomPrasol@users.noreply.github.com>
2024-02-03 21:27:00 +0300

    Update task2_3_4.py
    Задача 2

--- labor1/task2_3_4.py
+++ labor1/task2_3_4.py
@@ -6,12 +6,25 @@ def random_str(x):
     y = "".join(char)
     return y
 
-
+def pal_up_true(x):
+    for char in str(x):
+        if(char.isdigit()):
+            return "В строке есть числа"
+    y = ""
+    for char in str(x):
+        if(char.isupper()):
+            y += str(char)
+    if (y == y[::-1]):
+        return "Палиндром"
+    else:
+        return "Не палиндром"
 
 var = int(input("Введите 1, 2 или 3, чтоб выбрать соответствующие функции (5,7,14): "))
 a = input("Введите строку: ")
 match var:
     case 1:
         print(random_str(a))
+    case 2: 
+        print(pal_up_true(a))
     case _:
         print("Вы не выбрали функцию")
