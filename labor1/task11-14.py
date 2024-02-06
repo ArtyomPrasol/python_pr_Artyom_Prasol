7d971e87208d204baee47046500daf49cfc4bc71
ArtyomPrasol <artyom.prasol@gmail.com>
2024-02-06 16:54:23 +0300

    Задача 13
    Добавлена функция 13 (9)

--- labor1/task11-14.py
+++ labor1/task11-14.py
@@ -9,10 +9,30 @@ def sort_mid_weigt(x):
     mas_weight,x = zip(*sorted(zip(mas_weight, x)))
     return x
 
+def func3(x):
+    mas_razn = []
+    
+    for stri in x:
+        razn = 0
+        max_ord = 0
+        for ch in stri:
+            if(max_ord<ord(ch)):
+                max_ord = ord(ch)
+        for i in range(int(len(stri)/2)):
+            count = 0
+            if(ord(stri[i])==ord(stri[len(stri)-i-1])):
+                count += 1
+                razn += (ord(stri[i])-max_ord)**2
+        mas_razn.append((razn/count)**(1/2))
+
+    mas_razn, x = zip(*sorted(zip(mas_razn,x)))
+    return x
+
+
 n = input("Введите количество строк: ")
 list_str = []
 for i in range(int(n)):
     print(i+1)
     strok = input("Введите строку: ")
     list_str.append(strok)
-print(sort_mid_weigt(list_str))
+print(func3(list_str))
