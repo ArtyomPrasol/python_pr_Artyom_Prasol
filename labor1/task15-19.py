c797550c2a3b2a15a017efba4cdb2a04561b99bd
ArtyomPrasol <artyom.prasol@gmail.com>
2024-02-06 18:36:50 +0300

    Задача 16
    Написана функция 16 (23)

--- labor1/task15-19.py
+++ labor1/task15-19.py
@@ -2,10 +2,24 @@ def find_except(x):
     for i in x:
         if(x.count(i)==1):
             return i
+        
+
+def find_two_min_elem(x):
+    min = float('inf')
+    for i in x:
+        if (min>i):
+            min = i
+
+    count_f = min + 1
+    while (True):
+        if(x.count(count_f)):
+            return [min, count_f]
+        count_f += 1
+
 
 n = int(input("Введите количество элементов в массиве: "))
 mas =[]
 for i in range(n):
     print(i+1)
     mas.append(int(input("Введите элемент: ")))
-print(find_except(mas))
+print(find_two_min_elem(mas))
