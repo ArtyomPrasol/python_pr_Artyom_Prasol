2ba9be970f07f7c29a5037ea1f7685ac69fb6404
ArtyomPrasol <artyom.prasol@gmail.com>
2024-02-06 18:56:14 +0300

    Задание 17
    Написана функция 17 (35)

--- labor1/task15-19.py
+++ labor1/task15-19.py
@@ -16,10 +16,24 @@ def find_two_min_elem(x):
             return [min, count_f]
         count_f += 1
 
+def find_R_mas(x):
+    R = int(input("Введите число для поиска: "))
+    if(x.count(R)):
+        return R
+    up = R + 1
+    down = R - 1
+    while (True):
+        if(x.count(up)):
+            return up
+        if(x.count(down)):
+            return down
+        up += 1
+        down -= 1
+
 
 n = int(input("Введите количество элементов в массиве: "))
 mas =[]
 for i in range(n):
     print(i+1)
     mas.append(int(input("Введите элемент: ")))
-print(find_two_min_elem(mas))
+print(find_R_mas(mas))
