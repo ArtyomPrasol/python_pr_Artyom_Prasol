2ae8893dbd5b4cd1931ee9b4612d9c347002cb9e
ArtyomPrasol <artyom.prasol@gmail.com>
2024-02-20 22:00:30 +0300

    Лаб 6
    Добавлены строки в таблицы

--- labor6/task1.py
+++ labor6/task1.py
@@ -36,15 +36,9 @@ cursor = db.cursor()
 
 # cursor.execute("ALTER TABLE Client ADD age INTEGER NOT NULL")
 
-# cursor.execute("""
-# INSERT INTO Client (client_name, adress, phone_number, age) VALUES ('Ваня','ул. Пашковская',891834567,23),('Дима','ул. Кировская',12315125612,29),('Саша','ул. Зиповская',89005553535,21)      
-# """)
-# cursor.execute("""
-# INSERT INTO Subscription (name, price, num_days) VALUES ('Недельный', 1500, 7), ('Месяц', 4000, 30), ('Годовой', 8000, 365)     
-# """)
-
-em = db.execute("SELECT * FROM Client")
-print(em.fetchall())
+# cursor.execute("INSERT INTO Client (client_name, adress, phone_number, age) VALUES ('Ваня','ул. Пашковская',891834567,23),('Дима','ул. Кировская',12315125612,29),('Саша','ул. Зиповская',89005553535,21)")
+# cursor.execute("INSERT INTO Subscription (name, price, num_days) VALUES ('Недельный', 1500, 7), ('Месяц', 4000, 30), ('Годовой', 8000, 365)")
+# cursor.execute("INSERT INTO Saled_subs (id_client, id_sub, date_start, date_end) VALUES (1,1,2024-02-18, 2024-02-25), (2,1,2024-02-11, 2024-02-18), (3,2,2024-02-05,2024-03-06)")
 db.commit()
 
 db.close()
