109c166a46dfbf8a9d5b1a71a516c054af41198f
ArtyomPrasol <artyom.prasol@gmail.com>
2024-02-21 11:40:26 +0300

    Лаб 6
    Добавлены запросы

--- labor6/task1.py
+++ labor6/task1.py
@@ -38,7 +38,32 @@ cursor = db.cursor()
 
 # cursor.execute("INSERT INTO Client (client_name, adress, phone_number, age) VALUES ('Ваня','ул. Пашковская',891834567,23),('Дима','ул. Кировская',12315125612,29),('Саша','ул. Зиповская',89005553535,21)")
 # cursor.execute("INSERT INTO Subscription (name, price, num_days) VALUES ('Недельный', 1500, 7), ('Месяц', 4000, 30), ('Годовой', 8000, 365)")
-# cursor.execute("INSERT INTO Saled_subs (id_client, id_sub, date_start, date_end) VALUES (1,1,2024-02-18, 2024-02-25), (2,1,2024-02-11, 2024-02-18), (3,2,2024-02-05,2024-03-06)")
-db.commit()
+# cursor.execute("INSERT INTO Saled_subs (id_client, id_sub, date_start, date_end) VALUES (1,1,'2024-02-18', '2024-02-25'), (2,1,'2024-02-11', '2024-02-18'), (3,2,'2024-02-05','2024-03-06')")
+
+#db.commit()
+var = int(input("Введите 1-3 для выбора запроса: "))
+match(var):
+    case 1:
+        ce = cursor.execute("""
+SELECT Client.id, Client.client_name FROM Client
+                        JOIN Saled_subs on Client.id = Saled_subs.id_client
+                        WHERE Saled_subs.Date_end>=DATE()
+""")
+        print(ce.fetchall())
+    case 2:
+        ce = cursor.execute("""
+SELECT SUM(price) FROM Subscription
+                        JOIN Saled_subs ON Subscription.id = Saled_subs.id_sub
+                        WHERE Saled_subs.Date_end>=DATE()   
+""")
+        print(ce.fetchall()[0][0])
+    case 3:
+        ce = cursor.execute("""
+SELECT id_card, date_end FROM Saled_subs
+                        WHERE Date_end<=DATE()
+""")
+        print(ce.fetchall())
+    case _:
+        print("Запрос не был выбран!")
 
 db.close()
