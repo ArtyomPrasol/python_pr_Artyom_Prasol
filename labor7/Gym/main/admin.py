8545940ac7ac61989c45e95492d35f1952624209
ArtyomPrasol <artyom.prasol@gmail.com>
2024-03-26 18:03:08 +0300

    Лаб7
    Задача 2. Добавлены таблицы и данные в них

---
+++ labor7/Gym/main/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Sailed_subs, Subscription, Client
+# Register your models here.
+admin.site.register(Sailed_subs)
+admin.site.register(Subscription)
+admin.site.register(Client)
