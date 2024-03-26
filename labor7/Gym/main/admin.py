b25dbe2fe7bb5b9459a2a341a9f681f79f1ba123
ArtyomPrasol <artyom.prasol@gmail.com>
2024-03-26 18:26:19 +0300

    Лаб7
    Задача 3

--- labor7/Gym/main/admin.py
+++ labor7/Gym/main/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
 from .models import Sailed_subs, Subscription, Client
 # Register your models here.
-admin.site.register(Sailed_subs)
-admin.site.register(Subscription)
-admin.site.register(Client)
+class ModelClient(admin.ModelAdmin):
+    list_display = ['cl_name','adress','phone_num','age']
+    list_filter = ['cl_name', 'age']
+
+class ModelSubs(admin.ModelAdmin):
+    list_display = ['name', 'price', 'num_days']
+    list_filter = ['name', 'num_days']
+
+class ModelSailed(admin.ModelAdmin):
+    list_display = ['sub_id', 'cl_id', 'date_start', 'date_end']
+    list_filter = ['sub_id', 'date_start', 'date_end']
+
+admin.site.register(Sailed_subs, ModelSailed)
+admin.site.register(Subscription, ModelSubs)
+admin.site.register(Client, ModelClient)
