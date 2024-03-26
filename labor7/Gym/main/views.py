ece509569b2255d5daf0b1667d28994187c3a237
ArtyomPrasol <artyom.prasol@gmail.com>
2024-03-26 18:55:43 +0300

    Лаб7
    Задача 4

--- labor7/Gym/main/views.py
+++ labor7/Gym/main/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-
+from .models import Client,Sailed_subs,Subscription
 # Create your views here.
 def get_page(req):
-    return render(req, "form.html")
+    client_data = Client.objects.all()
+    subs_data = Subscription.objects.all()
+    sailed_data = Sailed_subs.objects.all()
+    return render(req, "form.html",{'client_data':client_data, 'subs_data': subs_data, 'sailed_data':sailed_data})
