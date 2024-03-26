4231ac178c2bfad9a387f443733eafa3b4499182
ArtyomPrasol <artyom.prasol@gmail.com>
2024-03-26 20:05:53 +0300

    Лаб7
    Задача 5

--- labor7/Gym/main/urls.py
+++ labor7/Gym/main/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import get_page
+from .views import get_page, add_client
  
 urlpatterns = [
-	path("", get_page, name="form")
-] 
+	path("", get_page, name="form"),
+    path('add/', add_client, name='add_client')
+]
  
--- labor7/Gym/main/views.py
+++ labor7/Gym/main/views.py
@@ -1,8 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Client,Sailed_subs,Subscription
+from .forms import ClientForm 
 # Create your views here.
 def get_page(req):
     client_data = Client.objects.all()
     subs_data = Subscription.objects.all()
     sailed_data = Sailed_subs.objects.all()
     return render(req, "form.html",{'client_data':client_data, 'subs_data': subs_data, 'sailed_data':sailed_data})
+
+def add_client(req):
+    if req.method == 'POST':
+        form = ClientForm(req.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('form')  # Перенаправление на главную страницу
+    else:
+        form = ClientForm()
+    
+    return render(req, 'add_client.html', {'form': form})
